cc191d6766f7d145cc9d6c7c684815f7e38b7d42
Kai <49048299+xiuwf@users.noreply.github.com>
2023-11-14 16:50:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_type = os.getenv("OPENAI_API_TYPE")
-openai.api_base = os.getenv("OPENAI_API_BASE")
-openai.api_version = os.getenv("OPENAI_API_VERSION")
+# openai.api_type = os.getenv("OPENAI_API_TYPE")
+# openai.api_base = os.getenv("OPENAI_API_BASE")
+# openai.api_version = os.getenv("OPENAI_API_VERSION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 gua_dict = {
